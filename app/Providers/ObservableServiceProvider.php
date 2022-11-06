229bf85606923781275a2463cf3b45cc561527ca
Tim Davarch <tim@davarch.ru>
2022-11-06 20:21:49 +0500

    user observer and observable service provider

---
+++ app/Providers/ObservableServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Models\User;
+use App\Observers\UserObserver;
+use Illuminate\Support\ServiceProvider;
+
+final class ObservableServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        User::observe(classes: UserObserver::class);
+    }
+}
